426df03c9da4a9dd49873e9114ab2ff386f80b96
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-10-19 12:36:52 -0400

    Update spam.js

--- spam.js
+++ spam.js
@@ -1,4 +1,21 @@
-//Without sleep
+// hack the world
 const KahootSpam = require('kahoot-spam')
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('What is the kahoot code', code => {
+  readline.close();
+});
+
+readline.question('What is the name of the bot', name => {
+  readline.close();
+});
+
+readline.question('What is the number of bots', numofbots => {
+  readline.close();
+});
+
 let api = KahootSpam
-api.spamWithAnswers(123456, "Name", 10, false)
+api.spamWithAnswers(code, name, numofbots, false)
