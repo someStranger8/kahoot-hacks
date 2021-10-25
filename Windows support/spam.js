5b94381a4062aea709cfe08a3d64b95e49217371
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-10-25 12:30:58 -0400

    Create spam.js

---
+++ Windows support/spam.js
@@ -0,0 +1,15 @@
+/*
+ Time to hack the world
+ Author: someStranger8
+*/
+
+const KahootSpam = require('kahoot-spam')
+
+// varibles
+var code = 1111111;
+var name = "name";
+var numbots = 10;
+
+// spam bots
+let api = KahootSpam
+api.spamWithAnswers(code, name, numbots, false)
