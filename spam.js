9d69f9cc3bc565e8662f085f5d1b9fdaa15ccd89
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-12-01 12:11:19 -0500

    Update spam.js

--- spam.js
+++ spam.js
@@ -7,8 +7,8 @@ const KahootSpam = require('kahoot-spam')
 var code = 1111111;
 var name = "name";
 var numbots = 10;
-let api = KahootSpam
 var i = 1;
 
 // spam bots with answers
+let api = KahootSpam
 while (i < 10) return api.spamWithAnswers(code, name, numbots, true);
